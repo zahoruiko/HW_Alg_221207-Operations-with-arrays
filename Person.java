2c100d8f0a0c2d1acba5464256ad93bee6b905c2
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2022-12-11 18:05:00 +0100

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Person implements Comparable<Person> {
+
+    private int age;
+    private String name;
+
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Person p)
+    {
+        if (this.age != p.getAge()) {
+            return this.age - p.getAge();
+        }
+        return this.name.compareTo(p.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Person))
+            return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() && Objects.equals(getName(), person.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAge(), getName());
+    }
+}
