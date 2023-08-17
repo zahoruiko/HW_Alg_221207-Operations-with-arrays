6b2b740c6e12e8105b02d37b4da4ee0519b54164
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2023-08-17 11:28:09 +0200

    Update ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -5,75 +5,72 @@ public class ArrayOperations {
     public static void main(String[] args) {
 
 
-        System.out.println("------------   Сотрировка пузырьком   --------------");
+        System.out.println("------------   Bubble sorting   --------------");
         int [] arr = {11, 3, 14, 16, 7};
-        System.out.println("Массив для сортировки методом пузырька: " + Arrays.toString(arr));
+        System.out.println("Array for sorting by the bubble method: " + Arrays.toString(arr));
         int[] arrSortResult = arraySort(arr);
-        System.out.println("Результат сортировки методом пузырька: " + Arrays.toString(arrSortResult));
+        System.out.println("The result of sorting the bubble method: " + Arrays.toString(arrSortResult));
 
 
-        System.out.println("----------------   Бинарный поиск   ----------------");
+        System.out.println("----------------   Binary search   ----------------");
         int valueToFind = 3;
         int[] valuesForBinarySearch = {1, 1, 2, 3, 4, 10};
-        System.out.println("Массив для выполнения бинарного поиска = " + Arrays.toString(valuesForBinarySearch));
-        System.out.println("Искомое значение = " + valueToFind);
+        System.out.println("Array for binary search = " + Arrays.toString(valuesForBinarySearch));
+        System.out.println("The desired value = " + valueToFind);
         int valueIndex = getIndexByValue(valuesForBinarySearch, valueToFind, 0, valuesForBinarySearch.length - 1);
-        System.out.printf("Индекс искомого значения = %d%n", valueIndex);
+        System.out.printf("Index of the desired value = %d%n", valueIndex);
 
 
-        System.out.println("--------   Получение элемента по индексу   ---------");
-        System.out.println("Массив для получения элемента по индексу: " + Arrays.toString(arr));
+        System.out.println("--------   Getting an item by index   ---------");
+        System.out.println("Array for getting an element by index: " + Arrays.toString(arr));
         int index = 2;
-        System.out.println("Индекс искомого элемента = " + index);
+        System.out.println("Index of the desired element = " + index);
         try {
             int valueFromArray = getValueByIndex(arr, index);
-            System.out.println("Значение, полученное по индексу = " + valueFromArray);
+            System.out.println("The value obtained by the index = " + valueFromArray);
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
 
 
-        System.out.println("-----   Создание массива увеличенной емкости   -----");
-        System.out.println("Исходный массив: " + Arrays.toString(arr));
-        System.out.println("Длина исходного массива: " + arr.length);
+        System.out.println("-----   Creating an array of increased capacity   -----");
+        System.out.println("Source array: " + Arrays.toString(arr));
+        System.out.println("Length of the source array: " + arr.length);
         int numberOfValues = 10;
-        System.out.println("Количество добавляемых ячеек: " + numberOfValues);
+        System.out.println("Number of cells to be added: " + numberOfValues);
         int[] elongatedArray = increaseArrayCapacity(arr, numberOfValues);
-        System.out.println("Массив увеличенной емкости: " + Arrays.toString(elongatedArray));
-        System.out.println("Длина увеличенного массива: " + elongatedArray.length);
+        System.out.println("An array of increased capacity: " + Arrays.toString(elongatedArray));
+        System.out.println("Length of the enlarged array: " + elongatedArray.length);
 
 
-        System.out.println("--------   Удаление элемента из массива   ---------");
-        System.out.println("Исходный массив: " + Arrays.toString(arr));
+        System.out.println("--------   Removing an element from an array   ---------");
+        System.out.println("Source array: " + Arrays.toString(arr));
         int indexOfDeletedElement = 2;
-        System.out.println("Индекс удаляемого значения: " + indexOfDeletedElement);
-        System.out.println("Удаляемое значение: " + arr[indexOfDeletedElement]);
+        System.out.println("Index of the value being deleted: " + indexOfDeletedElement);
+        System.out.println("Value to delete: " + arr[indexOfDeletedElement]);
         try {
             int[] truncatedArray = deleteValueFromArrayByIndex(arr, indexOfDeletedElement);
-            System.out.println("Укороченный массив: " + Arrays.toString(truncatedArray));
+            System.out.println("Shortened Array: " + Arrays.toString(truncatedArray));
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
 
-        System.out.println("--------   Добавление нового элемента в заданнцю позицию массива   ---------");
-        System.out.println("Исходный массив: " + Arrays.toString(arr));
+        System.out.println("--------   Adding a new element to the specified array position   ---------");
+        System.out.println("Source array: " + Arrays.toString(arr));
         int newValue = 100;
-        System.out.println("Добавляемое значение: " + newValue);
+        System.out.println("Value to add: " + newValue);
         int insertPosition = 3;
-        System.out.println("Индекс элемента для вставки: " + insertPosition);
+        System.out.println("Index of the element to insert: " + insertPosition);
         elongatedArray = addValueToArrayInPositionByIndex(arr, newValue, insertPosition);
-        System.out.println("Массив увеличенной емкости: " + Arrays.toString(elongatedArray));
-        System.out.println("Длина увеличенного массива: " + elongatedArray.length);
+        System.out.println("An array of increased capacity: " + Arrays.toString(elongatedArray));
+        System.out.println("Length of the enlarged array: " + elongatedArray.length);
         newValue = 100;
-        System.out.println("Добавляемое значение: " + newValue);
+        System.out.println("Added value: " + newValue);
         insertPosition = 10;
-        System.out.println("Индекс элемента для вставки: " + insertPosition);
+        System.out.println("Element index for insert: " + insertPosition);
         elongatedArray = addValueToArrayInPositionByIndex(arr, newValue, insertPosition);
-        System.out.println("Массив увеличенной емкости: " + Arrays.toString(elongatedArray));
-        System.out.println("Длина увеличенного массива: " + elongatedArray.length);
-
-
-
+        System.out.println("An array of increased capacity: " + Arrays.toString(elongatedArray));
+        System.out.println("The length of an increased array: " + elongatedArray.length);
     }
 
     public static int[] addValueToArrayInPositionByIndex(int[] sourceArray, int newValue, int insertPosition) {
@@ -82,18 +79,18 @@ public class ArrayOperations {
         if(insertPosition <= sourceArray.length - 1) {
             newArrayLength = sourceArray.length + 1;
         } else {
-            // Если позиция больше позиции последнего элемента
+            // If the position is greater than the position of the last element
             newArrayLength = insertPosition + 1;
         }
         int newArray[] = new int[newArrayLength];
         if(insertPosition <= sourceArray.length - 1) {
-            // Копируем элементы до позиции вставки
+            // Copy the elements to the insertion position
             for (int i = 0; i < insertPosition; i++) {
                 newArray[i] = sourceArray[i];
             }
-            // Вставляем новый элемент в заданную позицию
+            // Inserting a new element into the specified position
             newArray[insertPosition] = newValue;
-            // Копируем в новый массив остальные элементы из исходного массива
+            // Copy the remaining elements from the original array to the new array
             for (int i = insertPosition; i < sourceArray.length; i++) {
                 newArray[i + 1] = sourceArray[i];
             }
@@ -108,16 +105,16 @@ public class ArrayOperations {
 
     public static int[] deleteValueFromArrayByIndex(int[] arr, int index) throws Exception {
         if (index > arr.length - 1) {
-            throw new Exception("Введенный индекс превышает индекс последнего элемента");
+            throw new Exception("The entered index exceeds the index of the last element");
         }
-        // Создаем новый массив меньшей емкости
+        // Creating a new array of smaller capacity
         int[] newArray = new int[arr.length - 1];
-        // Копируем элементы от начала массива и до удаляемого элемента
+        // Copy the elements from the beginning of the array to the element being deleted
         int i;
         for(i = 0; i < index; i++) {
             newArray[i] = arr[i];
         }
-        // Копируем элементы после удаляемого значения из исходного массива
+        // Copy the elements after the value being deleted from the source array
         for(i = index + 1; i < arr.length; i++) {
             newArray[i - 1] = arr[i];
         }
@@ -133,7 +130,7 @@ public class ArrayOperations {
     }
 
     public static int getIndexByValue(int[] sortedArray, int valueToFind, int low, int high) {
-        // Бинарный поиск
+        // Binary search
         int index = -1;
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -150,15 +147,15 @@ public class ArrayOperations {
     }
 
     public static int getValueByIndex(int[] arr, int index) throws Exception {
-        // Получение элемента массива по его индексу
+        // Getting an array element by its index
         if (index > arr.length - 1) {
-            throw new Exception("Введенный индекс превышает индекс последнего элемента");
+            throw new Exception("The entered index exceeds the index of the last element");
         }
         return arr[index];
     }
 
     public static int[] arraySort(int[] arr) {
-        // Сортировка пузырьком
+        // Bubble Sorting
         boolean isSorted = false;
         int tmp;
         while(!isSorted) {
